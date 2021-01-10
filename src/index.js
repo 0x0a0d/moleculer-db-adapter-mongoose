b965e2ecd17c5526f878e4430ffbb246d2fc21dd
0x0a0d <vandug23@gmail.com>
2021-01-10 16:06:10 +0700

    fix bug

--- src/index.js
+++ src/index.js
@@ -90,15 +90,17 @@ class MongooseDbAdapter {
 					}).catch(reject);
 				}
 			} else if (this.schema) {
-				const conn = mongoose.createConnection(this.uri, this.opts)
-				conn.once('connected', () => {
-					this.model = conn.model(this.modelName, this.schema);
-					resolve(conn);
-				})
-				conn.once('error', reject)
+				// note: do not use then/catch here, some case will trigger bluebird warning
+				// https://github.com/petkaantonov/bluebird/blob/master/docs/docs/warning-explanations.md#warning-a-promise-was-created-in-a-handler-but-was-not-returned-from-it
+				resolve(mongoose.createConnection(this.uri, this.opts));
 			}
 		}).then(conn => {
+			if (this.model == null && this.schema) {
+				// if service's schema only has schema field type, not model, init model and return connection
+				this.model = conn.model(this.modelName, this.schema);
+			}
 			this.conn = conn;
+
 
 			if (this.conn.constructor.name === "Db") {
 				return this.conn;
